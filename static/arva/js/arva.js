d8cf0b648fd684522001dd9177e7318129b2e487
basukisabarcode <66512040+basukisabarcode@users.noreply.github.com>
2026-03-05 15:38:48 +0700

    Revert "feat : update code"
    This reverts commit 3aa32dac6264198a89828d2970a3fedf3ce82522.

--- static/arva/js/arva.js
+++ static/arva/js/arva.js
@@ -1415,12 +1415,6 @@ $(function() {
             const card = $(`.task-card[data-task-id='${taskId}']`);
             card.replaceWith(resp.html);
           }
-          if (resp.list_row_html) {
-            const row = $(`.task-list-row[data-task-id='${taskId}']`);
-            if (row.length) {
-              row.replaceWith(resp.list_row_html);
-            }
-          }
           if (typeof onSuccess === 'function') onSuccess();
         }
       },
